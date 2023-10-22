3d8a62e5a4ee50422da51c5b1183bed86f73795a
Nikita Ipatov <Nikitafilemine1@gmail.com>
2023-10-22 23:49:53 +0300

    feat: Добавлена валидация формы входа

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import Auth from '../ui/Auth/Auth';
 import { resources_ru } from '../../translations/resources_ru';
+import useForm from '../../utils/hooks/useForm';
 
 const Login = () => {
+
+  const logFormInitValue = {
+    email: '',
+    password: ''
+  };
+
+  const form = useForm(logFormInitValue);
+  const {formValue} = form;
+  const {handleFormChange} = form;
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+  };
+
   const inputFields = [
     {
       type: 'text',
-      name: 'email-input',
-      text: 'E-mail'
+      name: 'email',
+      text: 'E-mail',
+      value: '' || formValue.email,
+      validations: {
+        type: 'email',
+        required: true
+      }
     },
     {
       type: 'password',
-      name: 'password-input',
+      name: 'password',
       text: resources_ru.password,
+      value: '' || formValue.password,
+      validations: {
+        required: true,
+        minLength: 8
+      }
     }
   ];
 
@@ -22,8 +47,10 @@ const Login = () => {
       title={resources_ru.glad_to_see}
       submitBtnText={resources_ru.login}
       inputFields={inputFields}
+      handleFormChange={handleFormChange}
+      onSubmit={handleSignIn}
     />
   );
 };
 
-export default Login;
+export default React.memo(Login);
